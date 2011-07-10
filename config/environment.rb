25027583fdab0c310bdbda3e9b06b8007e6100ca
naoki kodama <naoki.koda@gmail.com>
2011-07-11 00:53:26 +0900

    設定値を削除。AppConfigに書く方針とする

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Todolist::Application.initialize!
 
-# 背景画像のパス
-BASE_BG_PATH = "/images/bg_img/"
-DEFAULT_BG_IMAGE = "viola.jpg"
-
-# レイアウトのパス
-BASE_LAYOUT_PATH = "/layout/"
-DEFAULT_LAYOUT  = "landscape"
-
-
-
