babb725b3c3421e2e15d76cae25b6f7688e886ae
Ryunosuke SATO <tricknotes.rs@gmail.com>
2011-09-19 23:44:22 +0900

    javascript の読み込みに application.js を利用するようにした。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,13 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// This is a manifest file that'll be compiled into including all the files listed below.
+// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
+// be included in the compiled file accessible from http://example.com/assets/application.js
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// the compiled file.
+//
+//= require jquery.min
+//= require jquery_ujs
+//= require jquery-ui
+//= require jquery.alerts
+//= require todo
+//= require p_timer
+//= require_tree .
