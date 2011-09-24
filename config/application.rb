50b282835c31e00df42986b21910edd18c39d9a5
naoki kodama <naoki.koda@gmail.com>
2011-09-25 02:26:52 +0900

    donelistの表示方法を変更。 でも、Viewでロジック組み過ぎなのでロジックに移行するか検討する。

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Todolist
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
