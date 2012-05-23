05169bdc30c4334da72be33c633f2e54e19caab2
naoki kodama <naoki.koda@gmail.com>
2012-05-23 13:29:19 +0900

    Add name to "from address"

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,5 +1,5 @@
 class TaskMailer < ActionMailer::Base
-  default from: "kanbanlist.info@gmail.com"
+  default from: "KanbanList<kanbanlist.info@gmail.com>"
 
   def all_tasks(user,book_name,addr,tasks)
     @user = user
