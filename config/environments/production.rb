b8be423f4d5557bc9e4726dc9f1cc109bf865947
naoki kodama <naoki.koda@gmail.com>
2012-05-23 12:49:42 +0900

    Add setting to production about send mail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,16 @@ Todolist::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'gmail.com',
+    :user_name => "kanbanlist.info@gmail.com",
+    :password => "kanbanlist_admin",
+    :authentication => 'plain',
+    :enable_starttls_auto => true,
+  }
 
   # Enable threaded mode
   # config.threadsafe!
