4e99261daa1087b99d0059b2687461a3d7d42d06
naoki kodama <naoki.koda@gmail.com>
2012-06-02 18:32:32 +0900

    Add auto link in case of existing URL in task.

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -293,7 +293,7 @@ function toggleDisplay(id1,id2) {
 }
  
 function updateToDoMsg(from, to) {
-  $(to).html($(from).get(0).value);
+  $(to).html(task_display_filter($(from).get(0).value));
 
   var id = to.slice(5);
   var status = $("#id_" + id).parent().get(0).id;
@@ -305,7 +305,8 @@ function updateToDoMsg(from, to) {
 function moveToDone(move_id) {
   var to_status = "done";
   var id = move_id.slice(4);
-  $("#fixed_msg_" + id ).html($("#msg_" + id ).html());
+  var msg = $("#ms_" + id + "_edit" ).val();
+  $("#fixed_msg_" + id ).html(task_display_filter(msg));
 
   $("#edit_link_ms_" + id ).css("display","none");
   $("#edit_form_ms_" + id ).css("display","none");
@@ -316,7 +317,6 @@ function moveToDone(move_id) {
 
   $('#viewSortlist').html("moveToDone " + move_id);
 
-  var msg = $("#msg_" + id).html();
   sendCurrentTodo(id, to_status, msg);
 }
 
@@ -333,7 +333,7 @@ function returnToTodo(ret_id){
 
   $('#viewSortlist').html("returnToTodo " + ret_id);
 
-  var msg = $("#msg_" + id).html();
+  var msg = $("#ms_" + id + "_edit" ).val();
   sendCurrentTodo(id, to_status, msg);
 }
 
@@ -408,6 +408,59 @@ function addTodoResponse(id, li_html, counts_json){
   }
 
   $(id_str).fadeIn();
+}
+
+function realize_task(id, msg_array){
+  var msg = msg_array.join('\n');
+
+  $('#ms_' + id + '_edit').val(msg);
+  $('#msg_' + id ).html(task_display_filter(msg));
+  $('#fixed_msg_' + id ).html(task_display_filter(msg));
+
+  $('#check_done_' + id).click(function(){
+    moveToDone('#id_' + id);
+    return false;
+  });
+
+  $('#check_return_' + id).click(function(){
+    returnToTodo('#id_' + id);
+    return false;
+  });
+
+  $('#delete_button_' + id ).click(function(){
+    deleteTodo('#id_' + id );
+    return false;
+  });
+
+  $('#fixed_delete_button_' + id ).click(function(){
+    deleteTodo('#id_' + id );
+    return false;
+  });
+
+  $('#edit_button_' + id ).click(function(){
+    $('#id_' + id ).parent().sortable('destroy');
+    var org_msg = $('#ms_' + id + '_edit').val();
+
+    toggleDisplay('edit_link_ms_' + id ,'edit_form_ms_' + id );
+    $('#ms_' + id + '_edit').get(0).focus();
+
+    $('#edit_form_' + id ).submit(function(){
+      $('#id_' + id ).parent().sortable(option);
+      updateToDoMsg('#ms_' + id + '_edit', '#msg_' + id );
+      toggleDisplay('edit_form_ms_' + id ,'edit_link_ms_' + id );
+      return false;
+    });
+
+    $('#edit_cancel_' + id ).click(function(){
+      $('#id_' + id ).parent().sortable(option);
+
+      $('#ms_' + id + '_edit').val(org_msg);
+      toggleDisplay('edit_form_ms_' + id ,'edit_link_ms_' + id );
+      return false;
+    });
+
+    return false;
+  });
 }
 
 function changeBgImg(img_name) {
@@ -513,7 +566,18 @@ function showMailResult(data){
   setTimeout(function(){
     $('#send_mail_result').fadeOut();
     },5000);
+}
 
-
+function task_display_filter(text){
+  var linked_text = text.replace(/((https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+))/g,
+      function(){
+        var matched_link = arguments[1];
+        if ( matched_link.match(/(\.jpg|\.gif|\.png|\.bmp)$/)){
+          return '<img src="' + matched_link + '"/>';
+        }else{
+          return '<a href="' + matched_link + '" target="_blank" >[URL]</a>';
+        }
+      });
+  return linked_text;
 }
 
