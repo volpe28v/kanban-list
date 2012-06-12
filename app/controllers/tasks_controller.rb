8f4d1c5bbae8b8e8d5f72a6924ad424cc582907d
naoki kodama <naoki.koda@gmail.com>
2012-06-13 00:46:24 +0900

    Fixed #11

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -88,7 +88,7 @@ class TasksController < ApplicationController
   private
   def get_book_id_in_msg(msg)
     #TODO: prefix と msg の分離は View でやるべき
-    if /^【(.+)】/ =~ msg
+    if /^【(.+?)】/ =~ msg
       current_user.books.find_by_name($1) || nil
     else
       return nil
