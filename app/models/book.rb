763ec00f78c373d834b249294c8aaa8341f87231
naoki kodama <naoki.koda@gmail.com>
2012-06-26 23:50:43 +0900

    Add counts to book list without new function.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ActiveRecord::Base
   belongs_to :user
   has_many :tasks
+
+  [:todo_h,:todo_m,:todo_l,:doing,:waiting,:done].each{|s|
+    define_method "#{s}_count" do
+      self.tasks.by_status(s).count
+    end
+  }
 end
