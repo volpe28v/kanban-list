eba8506012d84a490feb1346fad39eadca626e14
naoki kodama <naoki.koda@gmail.com>
2012-07-03 23:11:05 +0900

    Improve helper to_js_array include "[" "]"

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,5 @@
 module TasksHelper
   def to_js_array(msg)
-    raw msg.split("\n").map{|ts| "'" + ts + "'" }.join(",")
+    raw "[" + msg.split("\n").map{|ts| "'" + ts + "'" }.join(",") + "]"
   end
 end
