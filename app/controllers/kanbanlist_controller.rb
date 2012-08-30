d92f18f9ce99b8e7c4f98429b2a7f62b2969d41a
naoki kodama <naoki.koda@gmail.com>
2012-08-30 18:31:22 +0900

    Fix typo "require task".

--- app/controllers/kanbanlist_controller.rb
+++ app/controllers/kanbanlist_controller.rb
@@ -1,7 +1,7 @@
-#require 'Task'
+require 'task'
 class KanbanlistController < ApplicationController
   def index
     @all_user_count = User.count
-#    @all_task_count = Task.count
+    @all_task_count = Task.count
   end
 end
