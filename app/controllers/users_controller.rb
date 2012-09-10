348146345fa80d6077b8003f9cdd7cf6f7258635
naoki kodama <naoki.koda@gmail.com>
2012-09-10 11:16:42 +0900

    Extract UsersController#update_with_password and update_without_password.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,37 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
-  def index
-  end
-
-  def show
-  end
-
   def edit
-    @user = current_user;
+    @user = current_user
   end
 
   def update
     @user = User.find(params[:id])
-    if params[:user][:password].blank? and params[:user][:password_confirmation].blank?
-      if @user.update_without_password(params[:user])
-        redirect_to user_root_path
-      else
-        render :edit
-      end
+    if given_new_password_in_params?
+      update_with_password
     else
-      if @user.update_attributes(params[:user]) != true
-        render :edit
-      end
+      update_without_password
     end
   end
 
+  private
+  def given_new_password_in_params?
+    !params[:user][:password].blank? or !params[:user][:password_confirmation].blank?
+  end
+
+  def update_with_password
+    if @user.update_attributes(params[:user])
+      render :update
+    else
+      render :edit
+    end
+  end
+
+  def update_without_password
+    if @user.update_without_password(params[:user])
+      redirect_to user_root_path
+    else
+      render :edit
+    end
+  end
 end
