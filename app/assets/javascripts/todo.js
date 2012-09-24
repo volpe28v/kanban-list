0f4223befe3610f053c52844a215e6c1d830922d
naoki kodama <naoki.koda@gmail.com>
2012-09-24 13:28:18 +0900

    Change function name of markWithElem().

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -8,26 +8,26 @@ KanbanList.namespace('todayMarker');
 
 KanbanList.todayMarker = (function(){
 
-// 本日の編集した要素にマーカーをつける
-function markAll(){
-  markTodayEditWithElem( $('ul li span[id*="_time_"]:contains(' + utility.getTodayStr() + ')') );
-}
+  // 本日の編集した要素にマーカーをつける
+  function markAll(){
+    markWithElem( $('ul li span[id*="_time_"]:contains(' + utility.getTodayStr() + ')') );
+  }
 
-function markById( id ){
-  markTodayEditWithElem( $("#edit_link_time_" + id ) );
-  markTodayEditWithElem( $("#fixed_time_" + id ) );
-}
+  function markById( id ){
+    markWithElem( $("#edit_link_time_" + id ) );
+    markWithElem( $("#fixed_time_" + id ) );
+  }
 
-function markTodayEditWithElem( mark_obj ){
-  mark_obj.removeClass("label-info");
-  mark_obj.addClass("label-important");
-}
+  function markWithElem( mark_obj ){
+    mark_obj.removeClass("label-info");
+    mark_obj.addClass("label-important");
+  }
 
- return {
-   markAll: markAll,
-   markById: markById
- }
-   
+  return {
+    // public
+    markAll: markAll,
+    markById: markById
+  }
 }());
 
 
