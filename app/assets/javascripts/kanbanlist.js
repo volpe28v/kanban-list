861e424a3e5d48a297794e7f85882525bf634e67
naoki kodama <naoki.koda@gmail.com>
2012-09-26 00:53:47 +0900

    Change implement of global Kanbanlist valiable.

--- app/assets/javascripts/kanbanlist.js
+++ app/assets/javascripts/kanbanlist.js
@@ -1,22 +1,21 @@
-var KanbanList = KanbanList || {};
+var KanbanList = (function(){
+  function namespace(ns_string){
+    var parts = ns_string.split('.'),
+        object = this;
 
-KanbanList.namespace = function(ns_string){
-  var parts = ns_string.split('.'),
-      parent = KanbanList,
-      i;
-
-  if (parts[0] === "KanbanList"){
-    parts = parts.slice(1);
-  }
-
-  for (i = 0; i < parts.length; i += 1){
-    if (typeof parent[parts[i]] === "undefined"){
-      parent[parts[i]] = {};
+    for (var i = 0; i < parts.length; i += 1){
+      if (typeof object[parts[i]] === "undefined"){
+        object[parts[i]] = {};
+      }
+      object = object[parts[i]];
     }
-    parent = parent[parts[i]];
+    return object;
   }
-  return parent;
-};
+
+  return {
+    namespace: namespace
+  }
+}());
 
 
 
