db22213c428b35e7ad2445641e0c2fb5c7293176
naoki kodama <naoki.koda@gmail.com>
2012-10-10 00:29:07 +0900

    Add loading message about pomodoro technic.

--- app/assets/javascripts/ajax_loader.js
+++ app/assets/javascripts/ajax_loader.js
@@ -24,7 +24,10 @@ KanbanList.ajaxLoader = (function(){
     '<i class="icon-list"></i> で完了したタスクのリストページを表示します',
     '<i class="icon-envelope"></i> で表示しているリストをメール送信できます',
     '<i class="icon-picture"></i> で背景画像を設定できます',
-    '<i class="icon-refresh"></i> で自動更新機能の ON/OFF ができます'
+    '<i class="icon-refresh"></i> で自動更新機能の ON/OFF ができます',
+    'メニュー上の <img src="http://kanban-list.heroku.com/assets/tomato-c104ffb0220a27ca6d487fbb821ba1db.gif" /> をクリックするとポモドーロタイマーが使えます',
+    'ポモドーロ・テクニックを使ってこまめに作業のふりかえりをしましょう',
+    'ポモドーロ・テクニックは 25分のタイムボックスで区切って作業をします'
   ];
 
   function start(callback){
