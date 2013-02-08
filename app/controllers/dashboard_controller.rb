6f45f2ec6a04562543ddcd26dce6802b789fe662
naoki kodama <naoki.koda@gmail.com>
2013-02-09 00:28:55 +0900

    dummy

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,7 +5,8 @@ class DashboardController < ApplicationController
     @edit_tasks = @user.tasks.newest_edit
     @done_tasks = @user.tasks.newest_done
     @doing_tasks = @user.tasks.by_status(:doing).limit(10)
-    @today_tasks = @user.tasks.today_touch
+    @today_tasks = @user.tasks.today_done
+#    @today_tasks = @user.tasks.today_touch
     @books = books_count_info_array
     @task_counts = all_count_info
     @month_done_list = current_tasks.done_month_list.sort{|a,b| a[:date] <=> b[:date] }
