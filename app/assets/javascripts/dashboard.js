ccea56f1d2d83aa2b17487fade3af19392f0bafd
Naoki Kodama <naoki.koda@gmail.com>
2013-03-29 19:11:33 +0900

    Add dashboard.js for marking today edited task.

---
+++ app/assets/javascripts/dashboard.js
@@ -0,0 +1,11 @@
+//= require kanbanlist
+//= require today_marker
+//= require utility
+
+var utility = KanbanList.utility;
+var todayMarker = KanbanList.todayMarker;
+
+$(document).ready(function(){
+  todayMarker.markAll();
+  return;
+});
