abb0ac38c8ac9b1429ba2976509a6717c16c0d03
naoki kodama <naoki.koda@gmail.com>
2013-04-27 18:12:07 +0900

    Delete unmanaged specs.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,11 +2,4 @@ require 'spec_helper'
 
 describe DashboardController do
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
-    end
-  end
-
 end
